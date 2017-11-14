e58e4604550e30409e600e446fbac07df04def33
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2017-11-14 11:52:45 +0200

    rework config

--- lib/hm_crypto/public_key.ex
+++ lib/hm_crypto/public_key.ex
@@ -1,5 +1,5 @@
 defmodule HmCrypto.PublicKey do
-  def parse_pem(pem_string) when is_binary(pem_string) do
+  def parse_pem(pem_string) when is_binary(pem_string) and (pem_string != "") do
     [pem_entry] = :public_key.pem_decode(pem_string)
     :public_key.pem_entry_decode(pem_entry)
   end
